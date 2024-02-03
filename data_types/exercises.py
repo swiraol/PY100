1a3de62f2c1d0f00fcf1ce6a999e3ff4f04a2dc7
Robert Tsui <rt.roberttsui@gmail.com>
2024-02-02 20:48:28 -0800

    added #2, #3

--- data_types/exercises.py
+++ data_types/exercises.py
@@ -1,5 +1,5 @@
 # exercises
-# 1. 
+# identify the data type or class for each of the following values:
 'True' # String, 'str'
 False # Boolean, 'bool'
 (1, 2, 3) # Tuple, 'tuple'
@@ -10,3 +10,27 @@ range(5) # Range, 'range'
 {1, 2, 3} # Set, 'set'
 None # NoneType, 'NoneType'
 {'foo': 'bar'} # Dictionary, 'dict'
+
+# Create a tuple called names that contains several pet names. 
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+
+# Create a dictionary named pets that contains a list of pet names and the type of animal.
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+
+print(pets, {names})
